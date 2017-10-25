c6ddb0641b33a37b70094c424572f6483964e198
pinar.alper@uni.lu <pinar.alper@uni.lu>
2017-10-25 17:03:15 +0200

    Submission edit
    Editor label changed name unique cons added

--- elixir_dcp/models/submission.py
+++ elixir_dcp/models/submission.py
@@ -5,7 +5,7 @@ class Submission(db.Model):
     __tablename__ = 'submissions'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(60), index=True)
+    name = db.Column(db.String(60), index=True, unique=True, nullable=False)
     description = db.Column(db.String(60))
     created = db.Column(db.Date)
 
