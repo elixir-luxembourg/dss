56f11b5114790e1f4f7e861a52d8458edf604dde
Pinar Alper <pinar.alper@uni.lu>
2017-11-21 15:21:49 +0100

    Contact Type
    Added Contact Types for submission contacts

--- manage.py
+++ manage.py
@@ -7,7 +7,7 @@ from flask_migrate import MigrateCommand
 from flask_script import Manager, Shell
 
 from elixir_dcp import app, db
-from elixir_dcp.models import Submission, SubmissionContact
+from elixir_dcp.models import Submission, SubmissionContact, ContactType
 
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
@@ -18,13 +18,24 @@ def init_db():
     db.drop_all()
     db.create_all()
 
+    contact_type1 = ContactType(name='PI')
+    contact_type2 = ContactType(name='Researcher')
+    contact_type3 = ContactType(name='Data Manager')
+    contact_type4 = ContactType(name='Legal Representative')
+    contact_type5 = ContactType(name='Other')
+
+    db.session.add(contact_type1)
+    db.session.add(contact_type2)
+    db.session.add(contact_type3)
+    db.session.add(contact_type4)
+    db.session.add(contact_type5)
+    db.session.commit()
     sub1 = Submission(name='OncoTrack', description='Submission of Oncotrack data', created=datetime.today())
     sub2 = Submission(name='Predict-TB', description='Submission of Predict-TB preclinical data',
                       created=datetime.today())
-
-    contact1 = SubmissionContact(name='Pinar Alper', is_primary=True)
+    contact1 = SubmissionContact(name='Pinar Alper', category_id = 3, is_primary=True)
     sub1.contacts.append(contact1)
-    contact2 = SubmissionContact(name='Valentin Grouès', is_primary=True)
+    contact2 = SubmissionContact(name='Valentin Grouès', category_id = 1,  is_primary=True)
     sub2.contacts.append(contact2)
     db.session.add(sub1)
     db.session.add(sub2)
