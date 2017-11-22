47ba07c554886e0d29cac50bbf336deb7c641b1e
Valentin Grouès <valentin.groues@uni.lu>
2017-11-22 09:58:44 +0100

    simplify init_db script

--- manage.py
+++ manage.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-
 from datetime import datetime
 
 from flask_assets import ManageAssets
@@ -7,7 +6,7 @@ from flask_migrate import MigrateCommand
 from flask_script import Manager, Shell
 
 from elixir_dcp import app, db
-from elixir_dcp.models import Submission, SubmissionContact, ContactType
+from elixir_dcp.models import ContactType, Submission, SubmissionContact
 
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
@@ -18,24 +17,16 @@ def init_db():
     db.drop_all()
     db.create_all()
 
-    contact_type1 = ContactType(name='PI')
-    contact_type2 = ContactType(name='Researcher')
-    contact_type3 = ContactType(name='Data Manager')
-    contact_type4 = ContactType(name='Legal Representative')
-    contact_type5 = ContactType(name='Other')
-
-    db.session.add(contact_type1)
-    db.session.add(contact_type2)
-    db.session.add(contact_type3)
-    db.session.add(contact_type4)
-    db.session.add(contact_type5)
+    names_contact_types = ['PI', 'Researcher', 'Data Manager', 'Legal Representative', 'Other']
+    for name_contact_type in names_contact_types:
+        db.session.add(ContactType(name=name_contact_type))
     db.session.commit()
     sub1 = Submission(name='OncoTrack', description='Submission of Oncotrack data', created=datetime.today())
     sub2 = Submission(name='Predict-TB', description='Submission of Predict-TB preclinical data',
                       created=datetime.today())
-    contact1 = SubmissionContact(name='Pinar Alper', category_id = 3, is_primary=True)
+    contact1 = SubmissionContact(name='Pinar Alper', category_id=3, is_primary=True)
     sub1.contacts.append(contact1)
-    contact2 = SubmissionContact(name='Valentin Grouès', category_id = 1,  is_primary=True)
+    contact2 = SubmissionContact(name='Valentin Grouès', category_id=1, is_primary=True)
     sub2.contacts.append(contact2)
     db.session.add(sub1)
     db.session.add(sub2)
