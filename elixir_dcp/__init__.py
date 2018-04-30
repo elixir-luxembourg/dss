e89a5fb8fbee4c46fc84daedb4eb079124aa35f1
Pinar Alper <pinar.alper@uni.lu>
2018-04-30 11:50:35 +0200

    Added persistence of AAI credentials

--- elixir_dcp/__init__.py
+++ elixir_dcp/__init__.py
@@ -1,7 +1,6 @@
 import os
 import logging
 from logging.handlers import RotatingFileHandler
-
 from flask import Flask
 from flask_assets import Environment
 from flask_mail import Mail
@@ -13,6 +12,7 @@ from webassets.loaders import PythonLoader as PythonAssetsLoader
 import elixir_dcp.assets as assets
 import elixir_dcp.exceptions as exceptions
 from flask_oidc import OpenIDConnect
+import json
 
 __VERSION__ = "0.0.1-dev"
 
@@ -43,10 +43,6 @@ elif authentication_method == 'AAI':
 else:
     raise ValueError("Unsupported authentication method")
 
-
-oidc = OpenIDConnect()
-oidc.init_app(app)
-
 csrf = CSRFProtect()
 csrf.init_app(app)
 
@@ -58,11 +54,55 @@ for name, bundle in assets_loader.load_bundles().items():
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
+
+class OidcCredentials(db.Model):
+    __tablename__ = 'oidc_credentials'
+
+    cred_key = db.Column(db.String, primary_key=True)
+    cred_val = db.Column(db.String)
+
+
+class CredentialsPersistentStore:
+    def __init__(self, persist_db=None):
+        self.cred_db = persist_db
+
+    def __setitem__(self, key, val):
+        item = OidcCredentials.query.filter_by(cred_key=key).one_or_none()
+        if item:
+            item.cred_val = json.dumps(val)
+            self.cred_db.session.add(item)
+            self.cred_db.session.commit()
+        else:
+            item = OidcCredentials()
+            item.cred_key = key
+            item.cred_val = json.dumps(val)
+            self.cred_db.session.add(item)
+            self.cred_db.session.commit()
+
+    def __getitem__(self, key):
+        credential = OidcCredentials.query.filter_by(cred_key=key).one_or_none()
+        if credential:
+            return json.loads(credential.cred_val)
+        else:
+            return None
+
+    def __delitem__(self, key):
+        credential = OidcCredentials.query.filter_by(cred_key=key).one_or_none()
+        if credential:
+            db.session.delete(credential)
+            db.session.commit()
+        return
+
+
+credentials_store = CredentialsPersistentStore(persist_db=db)
+
+oidc = OpenIDConnect(app=app, credentials_store=credentials_store)
+
 # Setup Flask-Mail
 mail = Mail(app)
 
-
 app.add_template_global(login_manager.login_view, 'login_page')
+
 
 @app.template_filter('dt')
 def _jinja2_filter_datetime(date, fmt=None):
