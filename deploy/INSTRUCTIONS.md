d87db99173fafb0130f3b59bf14e84c7f7ca5c71
Pinar Alper <pinar.alper@uni.lu>
2018-08-08 15:34:56 +0200

    More install instruction bug fixes

--- deploy/INSTRUCTIONS.md
+++ deploy/INSTRUCTIONS.md
@@ -26,6 +26,7 @@ sudo passwd elixirdcp
 su elixirdcp
 
 mkdir app-data
+mkdir app-src
 mkdir app-data/uploads
 mkdir app-data/exports
 mkdir app-logs
@@ -33,6 +34,7 @@ mkdir app-logs
 
 ## Get the project
 ```bash
+cd app-src
 git clone ssh://git@git-r3lab-server.uni.lu:8022/elixir/elixir-dcp.git
 cd elixir-dcp
 
--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ except ImportError:
 requirements = [
     'Flask', 'Flask-Assets', 'Flask-SQLAlchemy', 'Flask-Script', 'Jinja2', 'SQLAlchemy', 'Werkzeug', 'closure',
     'Flask-Cache', 'Flask-Login', 'Flask-WTF', 'Flask-Migrate', 'flask-oidc', 'Flask-Testing',
-    'sqlalchemy-utils', 'Flask-Mail', 'Flask-Babel',     'cssmin', 'webassets',  'requests', 'pdfkit', 'flask_wkhtmltopdf'
+    'sqlalchemy-utils', 'Flask-Mail', 'Flask-Babel',     'cssmin', 'webassets',  'requests', 'pdfkit', 'flask_wkhtmltopdf',
     'WTForms-Components', 'schedule'
 ]
 
