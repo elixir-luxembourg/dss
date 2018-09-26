8d3ab6bb12867d03c7ed02410eb5a8ca8a9ae817
Pinar Alper <pinar.alper@uni.lu>
2018-09-26 12:26:25 +0200

    Bump version: 0.1.1-dev → 0.2.0-dev

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ test_requirements = [
 
 setup(
     name='elixir-dcp',
-    version='0.1.1-dev',
+    version='0.2.0-dev',
     description="Elixir-LU Data and Computing Platform",
     author="Valentin Grouès",
     author_email='valentin.groues@uni.lu',
