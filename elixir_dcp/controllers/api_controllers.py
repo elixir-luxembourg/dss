954d8460b8e768091953943dfd822d1272962e10
Pinar Alper <pinar.alper@uni.lu>
2020-01-03 11:13:32 +0100

    refactor of DAISY api client methods

--- elixir_dcp/controllers/api_controllers.py
+++ elixir_dcp/controllers/api_controllers.py
@@ -10,47 +10,33 @@ from elixir_dcp import app
 
 @app.cache.cached(timeout=1800, key_prefix='elu_partners')
 def get_elu_partners():
-    partners_json_str = None
-    try:
-        urlPartners = urllib.parse.urljoin(app.config.get('DAISY_URL'), '/api/partners')
-        with urllib.request.urlopen(urlPartners, timeout=5) as response:
-            try:
-                data_from_url = response.read().decode('utf-8')
-                json.loads(data_from_url)
-                partners_json_str = data_from_url
-            except ValueError as e:
-                app.logger.error('URL not returning valid Json: %s \nError: %s', urlPartners, e)
-    except (HTTPError, URLError) as error:
-        app.logger.error('Partner Data not retrieved from URL: %s, \nError: %s', urlPartners, error)
-    except timeout:
-        app.logger.error('Socket timed out from URL: %s', urlPartners)
-    finally:
-        if not partners_json_str:
-            app.logger.info('Defaulting config file')
-            partners_json_str = dumps(app.config.get('DATA_INIT')['collab_institutions'])
-    return json.loads(partners_json_str)
+    return get_elu_entities('partners')
 
 @app.cache.cached(timeout=1800, key_prefix='elu_cohorts')
 def get_elu_cohorts():
-    cohorts_json_str = None
+    return get_elu_entities('cohorts')
+
+
+def get_elu_entities(entity_name):
+    entities_json_str = None
     try:
-        urlCohorts = urllib.parse.urljoin(app.config.get('DAISY_URL'), '/api/cohorts')
-        with urllib.request.urlopen(urlCohorts, timeout=5) as response:
+        urlEntities = urllib.parse.urljoin(app.config.get('DAISY_URL'), '/api/'+ entity_name)
+        with urllib.request.urlopen(urlEntities, timeout=5) as response:
             try:
                 data_from_url = response.read().decode('utf-8')
-                json.loads(data_from_url)
-                cohorts_json_str = data_from_url
+                entities_dict = json.loads(data_from_url)
+                if 'results' not in entities_dict.keys():
+                    raise ValueError('results key not found')
+                entities_json_str = dumps(entities_dict['results'])
             except ValueError as e:
-                app.logger.error('URL not returning valid Json: %s \nError: %s', urlCohorts, e)
+                app.logger.error('URL not returning valid Json: %s \nError: %s', urlEntities, e)
     except (HTTPError, URLError) as error:
-        app.logger.error('Partner Data not retrieved from URL: %s, \nError: %s', urlCohorts, error)
+        app.logger.error('Data not retrieved from URL: %s, \nError: %s', urlEntities, error)
     except timeout:
-        app.logger.error('Socket timed out from URL: %s', urlCohorts)
+        app.logger.error('Socket timed out from URL: %s', urlEntities)
     finally:
-        if not cohorts_json_str:
+        if not entities_json_str:
             app.logger.info('Defaulting config file')
-            cohorts_json_str = dumps(app.config.get('DATA_INIT')['cohorts'])
-    return json.loads(cohorts_json_str)
-
-
+            entities_json_str = dumps(app.config.get('DATA_INIT')[entity_name])
+    return json.loads(entities_json_str)
 
