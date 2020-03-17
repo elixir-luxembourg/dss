0d2226ccead7d0645c79d9d40ec74e86daa55745
Pinar Alper <pinar.alper@uni.lu>
2020-03-17 17:09:43 +0100

    tests should not load lookup data from daisy

--- elixir_dcp/controllers/api_controllers.py
+++ elixir_dcp/controllers/api_controllers.py
@@ -19,24 +19,28 @@ def get_elu_cohorts():
 
 def get_elu_entities(entity_name):
     entities_json_str = None
-    try:
-        urlEntities = urllib.parse.urljoin(app.config.get('DAISY_URL'), '/api/'+ entity_name)
-        with urllib.request.urlopen(urlEntities, timeout=5) as response:
-            try:
-                data_from_url = response.read().decode('utf-8')
-                entities_dict = json.loads(data_from_url)
-                if 'results' not in entities_dict.keys():
-                    raise ValueError('results key not found')
-                entities_json_str = dumps(entities_dict['results'])
-            except ValueError as e:
-                app.logger.error('URL not returning valid Json: %s \nError: %s', urlEntities, e)
-    except (HTTPError, URLError) as error:
-        app.logger.error('Data not retrieved from URL: %s, \nError: %s', urlEntities, error)
-    except timeout:
-        app.logger.error('Socket timed out from URL: %s', urlEntities)
-    finally:
-        if not entities_json_str:
-            app.logger.info('Defaulting config file')
-            entities_json_str = dumps(app.config.get('DATA_INIT')[entity_name])
+    if app.config.get('DAISY_USE') is True:
+        try:
+            urlEntities = urllib.parse.urljoin(app.config.get('DAISY_URL'), '/api/'+ entity_name)
+            with urllib.request.urlopen(urlEntities, timeout=5) as response:
+                try:
+                    data_from_url = response.read().decode('utf-8')
+                    entities_dict = json.loads(data_from_url)
+                    if 'results' not in entities_dict.keys():
+                        raise ValueError('results key not found')
+                    entities_json_str = dumps(entities_dict['results'])
+                except ValueError as e:
+                    app.logger.error('URL not returning valid Json: %s \nError: %s', urlEntities, e)
+        except (HTTPError, URLError) as error:
+            app.logger.error('Data not retrieved from URL: %s, \nError: %s', urlEntities, error)
+        except timeout:
+            app.logger.error('Socket timed out from URL: %s', urlEntities)
+        finally:
+            if not entities_json_str:
+                app.logger.info('Defaulting config file')
+                entities_json_str = dumps(app.config.get('DATA_INIT')[entity_name])
+    else:
+        app.logger.info('Defaulting config file')
+        entities_json_str = dumps(app.config.get('DATA_INIT')[entity_name])
     return json.loads(entities_json_str)
 
