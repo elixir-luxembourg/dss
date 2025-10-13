3f29b7767a473c3bfcfa33964fe51b3c64055702
Moustapha Cheikh <moustapha.cheikh@uni.lu>
2025-10-13 09:34:27 +0200

    update frontend dependencies

--- setup_dev.sh
+++ setup_dev.sh
@@ -4,8 +4,8 @@ set -e
 python3 -m venv project_venv
 source ./project_venv/bin/activate
 pip install -e .[dev]
-cd elixir_dcp/static/vendor && npm ci && cd ../../../
-[ ! -f "elixir_dcp/settings.py" ] && cp elixir_dcp/settings.py.template elixir_dcp/settings.py
+cd elixir_dcp/static/vendor && npm ci && npm run build:css && cd ../../../
+[ ! -f "elixir_dcp/settings.py" ] && cp ./elixir_dcp/settings.py.template elixir_dcp/settings.py
 [ -f ".env.template" ] && [ ! -f ".env" ] && cp .env.template .env
 export FLASK_APP=elixir_dcp
 ./manage.py init-db
