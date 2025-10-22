5708ac8c4db802c26b0bae86cc76f41ce23eac6d
Amira KARCHOUD <amira.karchoud@unik7jtwkggj4.uni.lux>
2025-10-22 16:16:04 +0200

    fix keycloak auth

--- elixir_dss/controllers/utils.py
+++ elixir_dss/controllers/utils.py
@@ -1,17 +1,6 @@
 import hashlib
 
 
-def get_names_from_oidc(oidc_name):
-    result = ["", ""]
-    if oidc_name is not None:
-        if " " in oidc_name:
-            name_list = oidc_name.split(" ")
-            result[0] = name_list[0]
-            if len(name_list) > 1:
-                result[1] = name_list[1]
-    return result
-
-
 def equal_long_strings(str1: str, str2: str):
     if str1 is not None and str2 is not None:
         h1 = hashlib.sha1(str1.encode("utf-8"))
