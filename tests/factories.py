e83d1ef3ab99d51fe33d001ad5438056d4c824b1
Aleksandr KOZAREV <aleksandr.kozarev@unilxw1vp42mx.uni.lux>
2025-10-24 13:34:04 +0200

    update test

--- tests/factories.py
+++ tests/factories.py
@@ -5,7 +5,7 @@ class ProjectFactory(factory.Factory):
     class Meta:
         model = dict
 
-    external_id = factory.Sequence(lambda n: f"ELU_P_{n+1}")
+    external_id = factory.Sequence(lambda n: f"ELU_P_{n + 1}")
     name = factory.Faker("company")
     acronym = factory.LazyAttribute(lambda obj: obj.name[:3].upper())
 
@@ -14,6 +14,6 @@ class PartnerFactory(factory.Factory):
     class Meta:
         model = dict
 
-    external_id = factory.Sequence(lambda n: f"ELU_I_{n+1}")
+    external_id = factory.Sequence(lambda n: f"ELU_I_{n + 1}")
     name = factory.Faker("company")
     acronym = factory.LazyAttribute(lambda obj: obj.name[:3].upper())
