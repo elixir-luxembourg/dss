c85ff360dc46629cb1c13f06269a53fee30f9a26
Moustapha Cheikh <moustapha.cheikh@uni.lu>
2025-11-24 09:42:26 +0100

    fix formatting issues

--- elixir_dss/models/security.py
+++ elixir_dss/models/security.py
@@ -70,6 +70,10 @@ class Role(db.Model):
 class UsersRoles(db.Model):
     __tablename__ = "users_roles"
 
-    user_id = db.Column(db.Integer, db.ForeignKey("users.id", ondelete="CASCADE"), primary_key=True)
-    role_id = db.Column(db.Integer, db.ForeignKey("roles.id", ondelete="CASCADE"), primary_key=True)
+    user_id = db.Column(
+        db.Integer, db.ForeignKey("users.id", ondelete="CASCADE"), primary_key=True
+    )
+    role_id = db.Column(
+        db.Integer, db.ForeignKey("roles.id", ondelete="CASCADE"), primary_key=True
+    )
     assigned_on = db.Column("assigned_on", db.DateTime())
