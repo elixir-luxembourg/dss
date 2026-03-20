949492c7e08751431f04745f6dff17286ca73204
Amira KARCHOUD <amira.karchoud@unik7jtwkggj4.uni.lux>
2026-03-20 15:47:18 +0100

    removed unused code

--- elixir_dss/controllers/utils.py
+++ elixir_dss/controllers/utils.py
@@ -1,17 +1,3 @@
-import hashlib
-
-
-def equal_long_strings(str1: str, str2: str):
-    if str1 is not None and str2 is not None:
-        h1 = hashlib.sha1(str1.encode("utf-8"))
-        h2 = hashlib.sha1(str2.encode("utf-8"))
-        return h1.hexdigest() == h2.hexdigest()
-    elif str1 is None and str2 is None:
-        return True
-    else:
-        return False
-
-
 def dict_list_lookup(dict_list, search_key: str, search_val: str, target_key: str):
     for dict in dict_list:
         if search_key in dict and target_key in dict:
